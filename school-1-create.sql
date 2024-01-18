1e08b843785f7be9c2c349dc73c0145d4c6f92c3
Luke Packard <luke.packard@kellogg.northwestern.edu>
2024-01-18 01:13:14 +0000

    Designed school db

--- school-1-create.sql
+++ school-1-create.sql
@@ -1,7 +1,11 @@
 -- Create the students table
+.mode columns
+.headers on
+
 
 CREATE TABLE students (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   first_name TEXT,
   last_name TEXT
 );
+
---
+++ school-5-insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO students (
+    first_name, 
+    last_name, 
+    email) VALUES (
+        "Anthony",
+        "Spatafora",
+        "anthony@kellogg.northwestern.org"
+    );
